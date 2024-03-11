c2f72216d3f73840b689484e92981f3476ffc0c8
Arseny Drozdov <arsenii.drozdov@iss.digital>
2024-03-11 19:27:58 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import CompilerPluginSupport
 
 let package = Package(
     name: "Macros",
-    platforms: [.macOS(.v10_15), .iOS(.v14)],
+    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v12), .watchOS(.v6), .visionOS(.v1)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
