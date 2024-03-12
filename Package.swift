2be6574cd19419bd84ebe278011d9eff366a152c
Arseny <arsenijccom@gmail.com>
2024-03-12 20:50:45 +0300

    Bump version for tvOS to 13

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import CompilerPluginSupport
 
 let package = Package(
     name: "MockableMacros",
-    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v12), .watchOS(.v6), .visionOS(.v1)],
+    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .visionOS(.v1)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
